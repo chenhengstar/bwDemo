2efca3f192b68cb850a4ff7c5fd2329fd85deb6c
陈恒 <chenheng@chenhengdeMacBook-Pro.local>
2017-06-19 00:19:58 +0800

    a

---
+++ bw/bw/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bw
+//
+//  Created by 陈恒 on 2017/6/19.
+//  Copyright © 2017年 陈恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bw/bw/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bw
+//
+//  Created by 陈恒 on 2017/6/19.
+//  Copyright © 2017年 陈恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
